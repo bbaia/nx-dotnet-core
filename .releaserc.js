c317f71fa023235bfaad136ce6d232b6ef302466
Bruno Baia <brbaia@gmail.com>
2021-04-19 00:48:02 +0200

    build: add `semantic-release` for release management

---
+++ .releaserc.js
@@ -0,0 +1,49 @@
+const srcPackagePath = 'packages/nx-dotnet-core';
+const npmPackagePath = 'dist/packages/nx-dotnet-core';
+
+module.exports = {
+  branches: ['master', { name: 'beta', prerelease: true }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: [
+          `cp CHANGELOG.md ${npmPackagePath}`,
+          `cp README.md ${npmPackagePath}`,
+          `cp LICENSE ${npmPackagePath}`,
+        ].join(' && '),
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        pkgRoot: npmPackagePath,
+      },
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: `cp ${npmPackagePath}/package.json ${srcPackagePath}`,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', `${srcPackagePath}/package.json`],
+        message:
+          'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        failComment: false,
+        releasedLabels: false,
+        addReleases: 'top',
+      },
+    ],
+  ],
+};
